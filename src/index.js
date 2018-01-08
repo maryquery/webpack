dc7602b79c743d87844da1bab3f84fe68bc29885
Maria Ambroselli <maria.ambroselli@invia.de>
2018-01-08 14:01:10 +0100

    *004 . loading Data - output Management - Using Watch Moder + wepack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
  import _ from 'lodash';
+ import printMe from './print.js';
  import './style.css';
  import Icon from './ICON.jpg';
  import Data from './data.xml';
 
   function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -18,6 +20,12 @@
 
     //loading Data
     console.log(Data);
+
+    // prnit Output
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
   }
